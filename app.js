b30ee7918ac49728124dee5ac5dedf615d8119b1
waterfff <916501746@qq.com>
2018-11-24 21:57:34 +0800

    fix

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/controller/indexController');
+app.use('/controller/indexController' , indexxRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
