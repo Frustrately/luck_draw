fd9854d15c4b4536ee8bb65dca9eaf2f5631c85d
waterfff <916501746@qq.com>
2018-11-24 22:46:41 +0800

    test

--- routes/controller/indexController.js
+++ routes/controller/indexController.js
@@ -1,13 +1,12 @@
-var mysql      = require('mysql');
 var express = require('express');
 var router = express.Router();
+var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : '134.175.150.45',
   user     : 'luckdraw',
   password : 'luckdraw123',
   database : 'luck_draw'
-});
- 
+});	
 connection.connect();
 var  sql_get_draw = 'SELECT draw_name,create_time,award FROM draw';
 function get_draw(err, result){
@@ -17,8 +16,10 @@ function get_draw(err, result){
 	}
 	router.get('/get_draw_message',function (req, res) {
             res.send(result);
+			
     })
+	
 }
 connection.query(sql_get_draw,get_draw);
-
 connection.end();
+module.exports = router;
